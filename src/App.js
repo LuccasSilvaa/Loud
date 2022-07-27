407c2788760b974b8a4887d8303f5394537d9c1b
LuccasSilvaa <correaluccas@hotmail.com>
2022-07-27 17:56:05 -0300

    subindo projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Saci from './img/careca.jpg';
+import hack from './img/matias.jpg';
+import PANcada from './img/pANcada.jpg';
+import Aspas from './img/aspas.jpg';
+import Less from './img/less.jpg';
+import Slim from './img/eminem.jpg';
+import Logo from './img/LOUD.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+export default class App extends React.Component {
+
+  state = {
+    valorant: [
+      {
+        Player: "Sacy",
+        Age: 23,
+        agent: "Initiator",
+        img: Saci
+      },
+      {
+        Player: "Aspas",
+        Age: 19,
+        agent: "Duelist",
+        img: Aspas
+      },
+      {
+        Player: "Saadhak",
+        Age: 25,
+        agent: "Initiator/sentinel",
+        img: hack
+      },
+      {
+        Player: "pANcada",
+        Age: 22,
+        agent: "Controller",
+        img: PANcada
+      },
+      {
+        Player: "Less",
+        Age: 17,
+        agent: "sentinel",
+        img: Less
+      },
+      {
+        Player: "SlimShady",
+        Age: 20,
+        agent: "sentinel",
+        img: Slim
+      }
+    ]
+  }
+
+   render (){
+   return (
+    <div className="body">
+      <img className="logo" src={Logo} alt="Logo-Loud"/>
+      <div className="loudPlayers">
+     {this.state.valorant.map((item) => (
+        <section className="cards">
+        <ul className="info">
+          <img src={item.img}alt="player-loud"/>
+          <li>{item.Player}</li>
+          <li>{item.Age} age</li>
+          <li>{item.agent}</li>
+        </ul>
+        <button>Sobre</button>
+        </section>
+      ))}
+      </div>
     </div>
-  );
-}
+   );
+ }
 
-export default App;
+}
